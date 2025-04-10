9e30aa9681dcea8b1d495849f802db7f16219d37
Jeferson Oliveira <zuruckyt@gmail.com>
2025-04-09 22:09:52 -0300

    feat: adventurer

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -16,28 +16,42 @@ int main() {
         }   
     }
 
-    int ships[2][3][2] = {
+    int ships[4][3][2] = {
         {{0,0}, {0,1}, {0,2}},
-        {{5,6}, {6,6}, {7,6}}
+        {{5,6}, {6,6}, {7,6}},
+        {{1,9}, {2,8}, {3,7}},
+        {{2,3}, {3,4}, {4,5}}
     };
-
+    
     for (int i = 0; i < sizeof(ships) / sizeof(ships[0]); i++)
-    {
+    {        
         for (int j = 0; j < sizeof(ships[i]) / sizeof(ships[i][0]); j++)
         {
             int x = ships[i][j][0];
             int y = ships[i][j][1];
+
+            if (x > 9 || x < 0 || y > 9 || y < 0) {
+                printf("Erro: coordenadas fora do tabuleiro.\n");
+                return 1;
+            }
+            
             board[x][y] = 3;
         }
     }
     
     printf("Tabuleiro inicial:\n");
+    printf("X A B C D E F G H I J\n");
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
+            if (j == 0) {
+                printf("%d ", i);
+            }
+            
             int value = board[i][j];
             
+            // Print ships in red
             if (value == 3) {
                 printf("\033[31m");
                 printf("%d ", board[i][j]);
