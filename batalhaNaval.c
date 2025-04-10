8331ef109c0ba6e3c1c2eda1eaa6538b669ec7d1
Jeferson Oliveira <zuruckyt@gmail.com>
2025-04-09 21:29:29 -0300

    feat: beginner

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,42 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
+    int board[10][10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            board[i][j] = 0;
+        }   
+    }
+
+    int ships[2][3][2] = {
+        {{0,0}, {0,1}, {0,2}},
+        {{5,6}, {6,6}, {7,6}}
+    };
+
+    for (int i = 0; i < sizeof(ships) / sizeof(ships[0]); i++)
+    {
+        for (int j = 0; j < sizeof(ships[i]) / sizeof(ships[i][0]); j++)
+        {
+            int x = ships[i][j][0];
+            int y = ships[i][j][1];
+            board[x][y] = 3;
+        }
+    }
+    
+    printf("Tabuleiro inicial:\n");
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", board[i][j]); // Exibe o tabuleiro
+        }
+        printf("\n");
+    }
+    
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
